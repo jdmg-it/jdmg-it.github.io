6b6cd639a999408051b59761c9f6a0af59433846
José David Montoya Gallardo <jdmg@alu.ua.es>
2023-08-23 20:35:46 +0200

    About section

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jose David Montoya';
+export const SITE_DESCRIPTION = 'Bienvenido a mi portfolio, donde inicio mi travesía en el diseño web. Aunque soy nuevo en este campo, mi entusiasmo por crear y aprender se refleja en cada proyecto. Acompáñame mientras adquiero experiencia y desarrollo habilidades en la creación de interfaces atractivas y funcionales.';
